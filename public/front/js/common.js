f95592420ca2b4829a0e0f86edae98a9b2e44321
yxm0123 <771685455@qq.com>
2018-05-15 00:31:51 +0800

    搜索页完成

--- public/front/js/common.js
+++ public/front/js/common.js
@@ -15,3 +15,26 @@ $(function(){
     });
 });
 
+//获取本地存储的neir
+//    1.获取数据
+function getSearch(key){
+
+
+    var search = location.search;
+    //2.解码
+    search = decodeURI(search);
+    //删除问号
+    search = search.slice(1);
+    //将字符串转换成数组
+    var arr = search.splice('&');
+
+    var obj ={};
+    arr.forEach(function (ele,index) {
+
+        var k =ele.split("=")[0];
+        var v =ele.split("=")[1];
+        obj[ k ] = v ;
+    });
+    return obj[key]
+//---------------
+}
--- public/front/js/search.js
+++ public/front/js/search.js
@@ -22,7 +22,7 @@ $(function(){
         return arr;
     }
 
-    //获取本地储存历史 进行页面渲染
+    //2.获取本地储存历史 进行页面渲染
 
     function render(){
 
@@ -30,5 +30,79 @@ $(function(){
         console.log(arr);
         $(".lt_history").html(template("historyTemp",{ arr:arr }));
     }
+
+    //3.全部删除 点击删除
+    $('.lt_history').on("click",".btn_empty",function(){
+        console.log(1);
+        mui.confirm("你确认要清空所有历史记录么","温馨提示",["取消", "确认"],function(e){
+            console.log(e);
+            if(e.index === 1){
+                localStorage.removeItem("search_list");
+                render();
+            }
+        })
+    })
+
+    //4.删除一条数据
+    $('.lt_history').on("click",".btn_delete",function(){
+
+        var that = this;
+        mui.confirm("你确认要删除这条信息么?", "温馨提示", ["取消", "确认"],function(e){
+            if(e.index === 1){
+                //获取要删除的索引
+                var index = $(that).data("index");
+
+                //获取数组
+                var arr = getHistory();
+
+                //删除其内容
+                arr.splice( index,1 );
+                //将数组同步到localStorage 中
+                localStorage.setItem("search_list",JSON.stringify(arr));
+
+                render();
+            }
+        })
+    });
+
+    //5.添加内容
+    //思路：给input注册点击事件
+    // 获取input中的内容，
+    // 获取数组，添加到数组的最前面，
+    // 同步到数据库中 渲染页面
+
+    $(".lt_search button").click(function(){
+        //获取input内容
+        var val = $(".lt_search input").val();
+
+        //判断val
+        if(val === ""){
+            mui.toast('请输入关键字');
+            return;
+        }
+        var arr = getHistory();
+
+        //判断是否有重复的内容,如果有就删除
+        var index = arr.indexOf(val);
+        if(index > -1){
+            arr.splice(index,1)
+        }
+        //判断数组的长度
+        if(arr.length >= 10){
+            arr.pop();
+        }
+        //添加到数组的最前面
+        arr.unshift(val);
+
+        //同步到本地
+        localStorage.setItem("search_list",JSON.stringify(arr));
+
+        render();
+        //
+        $(".lt_search input").val("");
+
+        //跳转到搜索列表页，通过地址栏传入参数
+        location.href="searchList.html?val=" + val;
+    })
  //--------------------------------
 })
