788094a671353e82b93d8bab3b2ac33ca1ebfb02
yxm0123 <771685455@qq.com>
2018-05-17 23:58:29 +0800

    全部完成

--- public/front/js/member.js
+++ public/front/js/member.js
@@ -1,3 +1,37 @@
 /**
  * Created by yangxiaoman on 2018/5/16.
  */
+
+$(function(){
+
+  $.ajax({
+      type:'get',
+      url:'/user/queryUserMessage',
+      success:function(info){
+        console.log(info);
+
+        if(info.error===400){
+          location.href ="login.html";
+          return;
+        }
+
+        var htmlStr = template('userTemp',info);
+        $('#userInfo').html( htmlStr );
+
+      }
+  })
+
+  //2.点击退出
+  $('#logoutBtn').click(function(){
+    console.log(1);
+    $.ajax({
+      type:'get',
+      url:'/user/logout',
+      success:function(info){
+        console.log(info);
+        location.href = "login.html";
+      }
+    })
+  })
+  //-----------------
+})
